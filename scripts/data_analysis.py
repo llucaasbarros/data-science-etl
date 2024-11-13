4070f8091ebe14793b18e47a386f3058cab0e542
Lucas <lucasbarros.developer@gmail.com>
2024-11-13 10:21:55 -0300

    fix

--- scripts/data_analysis.py
+++ scripts/data_analysis.py
@@ -4,39 +4,25 @@ import pandas as pd
 file_path = './data/cleaned_data_science_job.parquet'
 data = pd.read_parquet(file_path)
 
-# Dicionário de mapeamento para expandir os níveis de experiência
-experience_mapping = {
-    'EN': 'Entry-level',
-    'MI': 'Mid-level',
-    'SE': 'Senior-level',
-    'EX': 'Expert-level'
-}
-
-# Dicionário de mapeamento para expandir os tipos de contrato
-employment_type_mapping = {
-    'FT': 'Full-time',
-    'PT': 'Part-time',
-    'CT': 'Contract',
-    'FL': 'Freelance'
-}
-
-# Substituir as abreviações na coluna 'experience_level' pelo texto completo
-data['experience_level'] = data['experience_level'].map(experience_mapping)
-
-# Substituir as abreviações na coluna 'employment_type' pelo texto completo
-data['employment_type'] = data['employment_type'].map(employment_type_mapping)
 
 # Contagem de candidatos por nível de experiência (ignora o tipo de emprego)
 experience_counts_no_employment_type = data.groupby('experience_level').size()
 print("\nContagem de Candidatos por Nível de Experiência (Ignorando Tipo de Emprego):")
 print(experience_counts_no_employment_type)
 
-# Análise adicional - Contagem de candidatos por localização e nível de experiência
+# Contagem de candidatos por localização e nível de experiência
 grouped_data_no_employment_type = data.groupby(['employee_residence', 'experience_level']).size().reset_index(name='count')
 print("\nDistribuição de Candidatos por Localização e Nível de Experiência (Ignorando Tipo de Emprego):")
 print(grouped_data_no_employment_type)
+
+# Análise da média salarial em dólar.
+salary_in_usd_mean = data['salary_in_usd'].mean()
+formatted_salary_mean = f"${salary_in_usd_mean:,.2f}"
+print('A média dos salários de todos os empregos na área de dados pagos em dólar é:', formatted_salary_mean, 'por ano.')
 
 # Salvar os resultados da análise em CSV para referência futura
 experience_counts_no_employment_type.to_csv('./data/experience_counts.csv', header=True)
 grouped_data_no_employment_type.to_csv('./data/grouped_data_no_employment_type.csv', index=False)
 print("Análises salvas nos arquivos 'experience_counts.csv' e 'grouped_data_no_employment_type.csv'.")
+
+
--- scripts/data_cleaning.py
+++ scripts/data_cleaning.py
@@ -1,33 +1,68 @@
 import pandas as pd
+from dotenv import load_dotenv
+import os
+from pymongo import MongoClient
 
-# Carrega o dataset com o caminho correto
+# Carregar variáveis de ambiente
+load_dotenv()
+
+# Caminho do arquivo CSV
 file_path = './data/data_science_job.csv'
-data = pd.read_csv(file_path)
 
-# Remove registros com valores zerados em colunas importantes
-data = data.dropna()
-data = data[data['salary'] > 0]
+# Verifica se o arquivo CSV existe
+if os.path.exists(file_path):
+    # Carrega o dataset
+    data = pd.read_csv(file_path)
 
-# Dicionários para mapeamento
-experience_mapping = {
-    'EN': 'Entry-level',
-    'MI': 'Mid-level',
-    'SE': 'Senior-level',
-    'EX': 'Expert-level'
-}
+    # Dicionários de mapeamento
+    experience_mapping = {
+        'EN': 'Entry-level',
+        'MI': 'Mid-level',
+        'SE': 'Senior-level',
+        'EX': 'Expert-level'
+    }
 
-employment_type_mapping = {
-    'FT': 'Full-time',
-    'PT': 'Part-time',
-    'CT': 'Contract',
-    'FL': 'Freelance'
-}
+    employment_type_mapping = {
+        'FT': 'Full-time',
+        'PT': 'Part-time',
+        'CT': 'Contract',
+        'FL': 'Freelance'
+    }
 
-# Remover espaços extras e aplicar os mapeamentos
-data['experience_level'] = data['experience_level'].str.strip().map(experience_mapping)
-data['employment_type'] = data['employment_type'].str.strip().map(employment_type_mapping)
-data['work_setting'] = data['work_setting'].str.strip().str.capitalize()
+    # 1. Limpeza dos Dados
+    data = data.dropna()  # Remove registros com valores zerados
+    data = data[data['salary'] > 0]  
 
-# Salvar os dados limpos em um arquivo Parquet para acesso rápido
-data.to_parquet('./data/cleaned_data_science_job.parquet', index=False)
-print("Dados limpos e salvos no arquivo 'cleaned_data_science_job.parquet'.")
+    # Remove espaços e valores que possam confundir a analise
+    data['experience_level'] = data['experience_level'].str.strip().map(experience_mapping)
+    data['employment_type'] = data['employment_type'].str.strip().map(employment_type_mapping)
+    data['work_setting'] = data['work_setting'].str.strip().str.capitalize()
+
+    # Salva dados limpos em Parquet
+    cleaned_file_path = './data/cleaned_data_science_job.parquet'
+    data.to_parquet(cleaned_file_path, index=False)
+    print("Dados limpos e salvos no arquivo 'cleaned_data_science_job.parquet'.")
+
+    # Conecta ao MongoDB
+    MONGODB_URL = os.getenv('MONGODB_URL')
+    if MONGODB_URL:
+        client = MongoClient(MONGODB_URL)
+        db = client['DataAnalisys']
+        collection = db['ParqueFiles']
+
+        # Utiliza o pandas pra ler o arquivo parquet salvo acima
+        data = pd.read_parquet(cleaned_file_path)
+
+        # Converter DataFrame para JSON-like
+        data_dict = data.to_dict("records")
+
+        # Inserir dados no MongoDB
+        try:
+            collection.insert_many(data_dict)
+            print("Dados do arquivo Parquet inseridos no MongoDB com sucesso!")
+        except Exception as e:
+            print("Erro ao inserir os dados no MongoDB:", e)
+    else:
+        print("A variável de ambiente MONGODB_URL não está definida.")
+else:
+    print(f"O arquivo '{file_path}' não foi encontrado.")
